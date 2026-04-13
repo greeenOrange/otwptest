8c7a53c58d9a2cba2ebb3871deace0424197faf9
greeenOrange <gm1sadiq@gmail.com>
2026-04-14 00:10:25 +0600

    tablet media added

--- task-1-html-css/js/script.js
+++ task-1-html-css/js/script.js
@@ -7,4 +7,19 @@ const swiper = new Swiper(".practicer_swiper", {
 
   // Optional smooth feel
   freeMode: false,
+  // Responsive (important)
+    breakpoints: {
+      0: {
+        slidesPerView: 1
+      },
+      576: {
+        slidesPerView: 2
+      },
+      768: {
+        slidesPerView: 3
+      },
+      1024: {
+        slidesPerView: 4
+      }
+    }
 });
