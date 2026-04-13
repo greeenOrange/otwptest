4a1315e767b2b7501939d95f640b37b88ce441f9
greeenOrange <gm1sadiq@gmail.com>
2026-04-13 13:50:46 +0600

    practicer section added

---
+++ task-1-html-css/js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".practicer_swiper", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+
+  // Enable drag/swipe
+  grabCursor: true,
+
+  // Optional smooth feel
+  freeMode: false,
+});
